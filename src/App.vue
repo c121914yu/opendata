9bfb549a668dc247cbb0b706c8fdf5a02e4b147d
jinlongyu <545436317@qq.com>
2020-01-16 19:25:20 +0800

    ui-remark Signed-off-by: jinlongyu <545436317@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,11 @@ export default {
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    font-size: 14px;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    line-height: 1.5;
+    letter-spacing: 1px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+/* axios */
+import axios from 'axios'
+Vue.prototype.$axios = axios//全局使用axios
+
+/* element-ui库 */
+import '../static/element/index.css' //自定义主题
+import Element from 'element-ui';
+Vue.use(Element, {zIndex: 3000 });
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
